0b7dba08e0fdc752af21c23fd8c0da77df5ebe08
jonstyle69 <44425507+jonstyle69@users.noreply.github.com>
2018-11-01 17:06:21 +0800

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -15,6 +15,7 @@ server_scripts {
 	'locales/es.lua',
 	'locales/sv.lua',
 	'locales/pl.lua',
+    'locales/tw.lua',
 	'config.lua',
 	'server/main.lua'
 }
@@ -29,6 +30,7 @@ client_scripts {
 	'locales/es.lua',
 	'locales/sv.lua',
 	'locales/pl.lua',
+    'locales/tw.lua',
 	'config.lua',
 	'client/main.lua'
 }
